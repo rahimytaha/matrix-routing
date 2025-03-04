d974d77404ba6ba482e507adb485827622531529
rahimytaha <rahimytaha9@gmail.com>
2025-03-04 03:02:35 -0800

    add copy for R

--- src/system/Checking.js
+++ src/system/Checking.js
@@ -1,4 +1,4 @@
-let allData = [1, 2, 3, 4]
+let allData = [1, 2, 3, 4,5]
 export function Checking(loc, data) {
     let founded_Routes = [];
     const startId = 1;
@@ -9,7 +9,7 @@ export function Checking(loc, data) {
         return { x: id, routes: row }
     })
     let firstRt = routes.find(el => el.x == startId);
-    choosing([1, 2,3]);
+    choosing([1, 2]);
 
 
 
@@ -21,25 +21,27 @@ export function Checking(loc, data) {
         let nextR = routes.find(el => el.x == target);
         // console.log(target,nextR.routes);
         nextR.routes = nextR.routes.filter(el => el[el.length - 1] > target);
-        // console.log(nextR)
+
         let cos = nextR.routes.map(el => {
             let length2 = el.length;
             let target2 = el[length2 - 1];
-            let nowData = R;
-            nowData[length2] = (target2);
+            
+            let nowData = [...R,target2  ]
+            console.log(nowData)
             // console.log(nowData, target2)
-            if (target2 == endId) {
-                // alert(nowData)
-                founded_Routes.push(nowData)
-            }
-            else{
-                console.log(nowData,"not ended")
-                choosing(nowData)
+            // if (target2 == endId) {
+            //     // alert(nowData)
+            //     founded_Routes.push(nowData)
+            // }
+            // else{
+            //     console.log(nowData,"not ended")
+            //     choosing(nowData)
 
-            }
+            // }
 
 
         })
+        console.log(R)
     }
 
     console.log(founded_Routes,"ended")
