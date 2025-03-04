c2290bcc9fbbd184630f0e3d62bb8f0190a3e62b
rahimytaha <rahimytaha9@gmail.com>
2025-03-04 02:20:31 -0800

    some change

--- src/system/Checking.js
+++ src/system/Checking.js
@@ -1,37 +1,47 @@
 let allData = [1, 2, 3, 4]
 export function Checking(loc, data) {
+    let founded_Routes = [];
     const startId = 1;
     const endId = 4;
     let routes = allData.map(id => {
         let row = allData.map(el => id != el && [id, el])
         row = row.filter(el => el != false);
-        // console.log(row,id);
         return { x: id, routes: row }
     })
-
     let firstRt = routes.find(el => el.x == startId);
-    // console.log(firstRt.routes);
+    choosing([1, 2,3]);
 
 
-    // firstRt.routes.map(el => choosing(el));
-    choosing([1, 2])
 
     function choosing(R) {
         let length = R.length;
         let target = R[length - 1]
+        console.log(target)
 
         let nextR = routes.find(el => el.x == target);
         // console.log(target,nextR.routes);
-        nextR.routes = nextR.routes.filter(el => el[el.length - 1] >target);
-        console.log(nextR)
-        // let cos = nextR.routes.map(el => {
-        //     let length2 = el.length;
-        //     let target2 = el[length2 - 1]
-        //     // let i = R.push(target2)
-        //     // console.log(i, R, el)
-        //     console.log(el)
-        // })
-        // console.log(cos)
+        nextR.routes = nextR.routes.filter(el => el[el.length - 1] > target);
+        // console.log(nextR)
+        let cos = nextR.routes.map(el => {
+            let length2 = el.length;
+            let target2 = el[length2 - 1];
+            let nowData = R;
+            nowData[length2] = (target2);
+            // console.log(nowData, target2)
+            if (target2 == endId) {
+                // alert(nowData)
+                founded_Routes.push(nowData)
+            }
+            else{
+                console.log(nowData,"not ended")
+                choosing(nowData)
+
+            }
+
+
+        })
     }
+
+    console.log(founded_Routes,"ended")
 }
 
